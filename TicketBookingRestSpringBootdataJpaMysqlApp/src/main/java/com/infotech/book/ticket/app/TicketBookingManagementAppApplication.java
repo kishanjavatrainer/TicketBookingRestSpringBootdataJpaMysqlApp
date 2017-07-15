d607ba31d6f12f76dd3c4193044b7aabce93e9ea
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-07-15 23:19:00 +0530

    TicketBookingRestSpringBootdataJpaMysqlApp Source code

---
+++ TicketBookingRestSpringBootdataJpaMysqlApp/src/main/java/com/infotech/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.infotech.book.ticket.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingManagementAppApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingManagementAppApplication.class, args);
+	}
+}
---
+++ TicketBookingRestSpringBootdataJpaMysqlApp/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO ticket(passenger_name,booking_date,source_station,dest_station,email) VALUES('Sean','2017/07/14','Patna','Delhi','sean.s2017@yahoo.com');
